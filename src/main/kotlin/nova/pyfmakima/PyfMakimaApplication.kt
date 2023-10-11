8f83ae71d1c7011562681473c9129cd9d72ab8dd
NovaFox161 <novamaday@gmail.com>
2023-10-10 21:51:27 -0500

    This should fix logging

--- src/main/kotlin/nova/pyfmakima/PyfMakimaApplication.kt
+++ src/main/kotlin/nova/pyfmakima/PyfMakimaApplication.kt
@@ -3,7 +3,9 @@ package nova.pyfmakima
 import nova.pyfmakima.config.Config
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Component
 
+@Component
 @SpringBootApplication
 class PyfMakimaApplication
 
